ee0df81d06cdf9008caf2c72847c0109c6109fb0
Mark Aves <markaves@gmail.com>
2015-08-11 17:45:04 +0800

    updated helloworld

--- UnitTesting/src/center/HelloWorld.java
+++ UnitTesting/src/center/HelloWorld.java
@@ -1,7 +1,7 @@
 package center;
 
 public class HelloWorld {
-	public static void main(String args[]) {
+	public static void main(String[] args) {
 		System.out.println("Program Running successfully");
 	}
 	public String concatinate(String one, String two){
